8baf0d820f20f81803674c6ea2524a9dc073f189
CHA-NYANN <patrahapsari@gmail.com>
2026-03-15 21:06:27 +0700

    keknya masalah bahasa dah mo selesai

--- apps/web/app/settings/account/components/preferences/ConfirmAdultDialog.tsx
+++ apps/web/app/settings/account/components/preferences/ConfirmAdultDialog.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { useUILanguageText } from "@/app/components/ui-language/UILanguageProvider";
 import { Button } from "@/components/ui/button";
 import {
@@ -19,25 +20,66 @@ type Props = {
 
 export default function ConfirmAdultDialog({ open, setOpen, onConfirm }: Props) {
   const t = useUILanguageText("Shared Dialogs");
+  const [step, setStep] = useState<1 | 2>(1);
+
+  function handleOpenChange(v: boolean) {
+    if (!v) setStep(1);
+    setOpen(v);
+  }
+
+  function handleIDontCare() {
+    setStep(2);
+  }
+
+  function handleFinalConfirm() {
+    setStep(1);
+    setOpen(false);
+    onConfirm();
+  }
+
+  function handleBack() {
+    setStep(1);
+    setOpen(false);
+  }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={handleOpenChange}>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{t("18+ Warning")}</DialogTitle>
-          <DialogDescription>
-            {t("By checking this box, you acknowledge that you are responsible for the content you choose to read. The developers are not responsible for what you access. This is your final warning.")}
-          </DialogDescription>
-        </DialogHeader>
-
-        <DialogFooter>
-          <Button variant="outline" type="button" onClick={() => setOpen(false)}>
-            {t("No, I do not want to continue.")}
-          </Button>
-          <Button type="button" onClick={onConfirm}>
-            {t("Yes, I understand.")}
-          </Button>
-        </DialogFooter>
+        {step === 1 ? (
+          <>
+            <DialogHeader>
+              <DialogTitle>{t("NSFW Warning")}</DialogTitle>
+              <DialogDescription>
+                {t("You are not allowed to unseal adult works if you are still a minor. Exit the settings and simply find works that are age-appropriate. There is nothing good about becoming an adult before your time. Even if you are an adult, unseal is still not recommended. Remember your Lord and leave this place; do not continue. The works that will appear if this seal is unseal will only increase your sins. It is time for you to repent and return to the right path.")}
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button variant="outline" type="button" onClick={() => handleOpenChange(false)}>
+                {t("I get it, thanks for reminding me.")}
+              </Button>
+              <Button type="button" onClick={handleIDontCare}>
+                {t("I don't care! Just open the seal!")}
+              </Button>
+            </DialogFooter>
+          </>
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>{t("NSFW Warning")}</DialogTitle>
+              <DialogDescription>
+                {t("I warned you, okay! If you don't heed my warning, then don't blame me or drag me to the court of justice in the afterlife. You must take full responsibility for yourself and the sins you've committed. I don't want any involvement in this.")}
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button variant="outline" type="button" onClick={handleBack}>
+                {t(".... After I thought about it, I decided not to.")}
+              </Button>
+              <Button type="button" onClick={handleFinalConfirm}>
+                {t("Okay, okay! Shut up! I know that! I won't involve you!")}
+              </Button>
+            </DialogFooter>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
--- apps/web/app/settings/account/components/preferences/ConfirmDeviantLoveDialog.tsx
+++ apps/web/app/settings/account/components/preferences/ConfirmDeviantLoveDialog.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { useUILanguageText } from "@/app/components/ui-language/UILanguageProvider";
 import { Button } from "@/components/ui/button";
 import {
@@ -19,25 +20,66 @@ type Props = {
 
 export default function ConfirmDeviantLoveDialog({ open, setOpen, onConfirm }: Props) {
   const t = useUILanguageText("Shared Dialogs");
+  const [step, setStep] = useState<1 | 2>(1);
+
+  function handleOpenChange(v: boolean) {
+    if (!v) setStep(1);
+    setOpen(v);
+  }
+
+  function handleIDontCare() {
+    setStep(2);
+  }
+
+  function handleFinalConfirm() {
+    setStep(1);
+    setOpen(false);
+    onConfirm();
+  }
+
+  function handleBack() {
+    setStep(1);
+    setOpen(false);
+  }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={handleOpenChange}>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{t("Deviant Love Warning")}</DialogTitle>
-          <DialogDescription>
-            {t("By checking this box, you confirm that you are not under any pressure and that you accept responsibility for what you choose to view. This content may be extreme.")}
-          </DialogDescription>
-        </DialogHeader>
-
-        <DialogFooter>
-          <Button variant="outline" type="button" onClick={() => setOpen(false)}>
-            {t("No, I do not want to continue.")}
-          </Button>
-          <Button type="button" onClick={onConfirm}>
-            {t("Yes, I understand.")}
-          </Button>
-        </DialogFooter>
+        {step === 1 ? (
+          <>
+            <DialogHeader>
+              <DialogTitle>{t("Deviant Love Warning")}</DialogTitle>
+              <DialogDescription>
+                {t("This work, which contains deviant love, clearly violates morals and social standards. The deviant love in question is a form of sexual deviance that is not a good idea to imitate. You are strongly advised not to open this seal, as viewing this type of work could lead you further away from being a normal human being. It's best to refrain from doing so.")}
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button variant="outline" type="button" onClick={() => handleOpenChange(false)}>
+                {t("I get it, thanks for reminding me.")}
+              </Button>
+              <Button type="button" onClick={handleIDontCare}>
+                {t("I don't care! Just open the seal!")}
+              </Button>
+            </DialogFooter>
+          </>
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>{t("Deviant Love Warning")}</DialogTitle>
+              <DialogDescription>
+                {t("I warned you, okay! If you don't heed my warning, then don't blame me or drag me to the court of justice in the afterlife. You must take full responsibility for yourself and the sins you've committed. I don't want any involvement in this.")}
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button variant="outline" type="button" onClick={handleBack}>
+                {t(".... After I thought about it, I decided not to.")}
+              </Button>
+              <Button type="button" onClick={handleFinalConfirm}>
+                {t("Okay, okay! Shut up! I know that! I won't involve you!")}
+              </Button>
+            </DialogFooter>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
